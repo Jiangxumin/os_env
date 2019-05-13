8812d646eee093cfecd122b50e3a46a7dea629ec
jiangxumin <jiangxumin@sdpmjd.com>
2019-05-13 16:17:07 +0800

    update ...

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -1,5 +1,13 @@
 apt-get install -y chromium-browser
 
+#####################################################
+# Indicator Stickynotes - Ubuntu 桌面便签小工具 
+#####################################################
+sudo add-apt-repository ppa:umang/indicator-stickynotes
+sudo apt-get update 
+sudo apt-get install indicator-stickynotes 
+
+
 apt-get install -y vlc
 #vlc rtsp://184.72.239.149/vod/mp4://BigBuckBunny_175k.mov
 
@@ -7,6 +15,12 @@ apt-get install git
 git config --global core.editor vim
 
 
-
 # git clone 
-git clone  https://sdpromote_postmaster@gitee.com/sdpromote/demo_server.git
+#git clone https://jiangxumin@github.com/my-rds-store/my_space.git  $HOME/Documents/my_space 
+
+mkdir $HOME/work
+git clone https://sdpromote_postmaster@gitee.com/sdpromote/demo_server.git  $HOME/work/demo_server
+
+# wps
+
+# pycharm 
--- ubuntu/test_rst.sh
+++ ubuntu/test_rst.sh
@@ -1,1 +1,2 @@
-vlc rtsp://184.72.239.149/vod/mp4://BigBuckBunny_175k.mov
+#vlc rtsp://184.72.239.149/vod/mp4://BigBuckBunny_175k.mov
+vlc rtsp://root:password@192.168.0.90/axis-media/media.amp
